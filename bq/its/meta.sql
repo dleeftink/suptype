29da1fc2fa3b3f4d61680445c5eecfb3648e9104
dleeftink <dennisleeftink@gmail.com>
2026-04-23 23:03:56 +1000

    Create its/meta.sql

---
+++ bq/its/meta.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE FUNCTION its.meta() AS (STRUCT(
+  "Conditionals: Control flow utilities for the 'suptype' namespace." AS scope,
+  "v0.0.0" AS version,
+  "https://github.com/dleeftink/suptype" AS repo
+)) OPTIONS (
+  description = "Control flow utilities for the 'suptype' namespace."
+);
